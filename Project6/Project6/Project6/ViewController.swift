250d5010551e78f0c8f6dc4dd01534ddd91450ab
Ian McDonald <ianjoemcdonald@gmail.com>
2020-03-29 12:30:45 -0300

    Project 6: Tutorial Complete

---
+++ Project6/Project6/Project6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project6
+//
+//  Created by Ian McDonald on 29/03/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
